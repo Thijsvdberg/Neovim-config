ded2d842ecdbced26bff3af6f1b63d3e2a7b023b
Thijs van den Berg <thijs@mountainwebs.nl>
2023-07-02 13:12:30 +0200

    omnisharp.lua

---
+++ lua/user/lsp/settings/omnisharp.lua
@@ -0,0 +1,12 @@
+local opts = {
+    enable_editor_support = true,
+    enable_ms_build_load_projects_on_demand = false,
+    enable_roslyn_analyzers = false,
+    organize_imports_on_format = false,
+    enable_import_completion = true,
+    sdk_include_prereleases = true,
+    analyze_open_documents_only = true,
+
+    filetypes = { 'cs', 'vb' },
+}
+return opts
